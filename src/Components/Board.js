98ecf67abbc205aea83c0465bf0600e975c7eac1
Dariell Vasquez <dariellv@yahoo.com>
2019-01-11 18:00:05 -0500

    working game

---
+++ src/Components/Board.js
@@ -0,0 +1,166 @@
+import React, {Component} from 'react';
+import Tile from './tile';
+import './Board.css';
+// Most commonly used screen resolutions
+const WIDTH = 1024;
+const HEIGHT = 728;
+// any multiple of 8 will provide a perferct square
+const TILE_SIZE = 32;
+
+export default class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.rows = Math.round(HEIGHT / TILE_SIZE);
+        this.columns = Math.round(WIDTH / TILE_SIZE);
+        this.board = this.makeEmptyBoard();
+        this.state = {
+            tiles: [],
+            interval: 500,
+            intervalId: null,
+            isRunning: false
+        }
+    }
+    // Create an empty board
+    makeEmptyBoard() {
+        let board = [];
+        // iterate over the each row
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = [];
+            // iterate over each tile in given column
+            for (let x = 0; x < this.columns; x++) {
+                // set tile to false   
+                board[y][x] = false;
+            }
+        }
+        return board;
+    }
+    makeRandomBoard = () => {
+        let board = [];
+        // iterate over the each row
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = [];
+            // iterate over each tile in given column
+            for (let x = 0; x < this.columns; x++) {
+                // set tile to true or false based on a ~50% chance
+                  
+                board[y][x] = (Math.random() >= .5);
+            }
+        }
+        this.board = board;
+        this.setState({tiles: this.createTiles()});
+    }
+    // Create tiles from this.board
+    createTiles() {
+        let tiles = [];
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.columns; x++) {
+                if (this.board[y][x]) {
+                    tiles.push({ x, y });
+                }
+            }
+        }
+        return tiles;
+    }
+    runGame = () => {
+        const intervalId = setInterval(this.evolve, this.state.interval);
+        this.setState({ 
+            isRunning: true,
+            intervalId
+        });
+    }
+    stopGame = () => {
+        clearInterval(this.state.intervalId);
+        this.setState({ isRunning: false, intervalId: null});
+    }
+    evolve = () => {
+        let newBoard = this.makeEmptyBoard();
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.columns; x++) {
+                // for each iteration, we need to find the total neighbors branching from all directions, including diagonal 
+                let neighbors = this.calculateNumNeighbors(x, y);
+                if (this.board[y][x]) {
+                    if (neighbors === 2 || neighbors === 3) {
+                        // Any live cell with two or three live neighbors lives on to the next generation.
+                        newBoard[y][x] = true;
+                    } else {
+                        // Any live cell with fewer than two live neighbors dies, as if caused by under population.
+                        // Any live cell with more than three live neighbors dies, as if by overpopulation.
+                        newBoard[y][x] = false;
+                    }
+                } else {
+                    if (!this.board[y][x] && neighbors === 3) {
+                        // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+                        newBoard[y][x] = true;
+                    }
+                }
+            }
+        }
+        this.board = newBoard;
+        // reinitialize the tiles at their new positions
+        this.setState({ tiles: this.createTiles() });   
+    }
+
+    calculateNumNeighbors(x, y) {
+        let neighbors = 0;
+        // [y, x] coordinates in relation to the current tile assuming we're at position [0, 0], this is clockwise starting from [-1,-1]
+        // top-left top-mid top-right mid-right bottom-right  bottom-middle bottom-left middle left 
+        const positions = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+        positions.forEach(position=>{
+            let [y1, x1] = position;
+            // shift positions according to neighbor
+            y1 += y;
+            x1 += x
+            // need this error-boundary because without it, we could be checking a tile that doesn't exist for neighbors, which would throw and error
+            if (y1 >= 0 && y1 < this.rows && x1 >= 0 && x1 < this.columns  && this.board[y1][x1]) {
+                neighbors++;
+            } 
+        }) 
+        return neighbors;
+    }
+
+    handleClick = event => {
+        // calculates the position of the board rectangle
+        const rect = this.boardRef.getBoundingClientRect();
+        const offsetX = event.clientX - rect.left;
+        const offsetY = event.clientY - rect.top;
+        const x = Math.floor(offsetX / TILE_SIZE);
+        const y = Math.floor(offsetY / TILE_SIZE);
+        // check if x and y are both in the bounds of the grid
+        if (x >= 0 && x <= this.columns && y >= 0 && y <= this.rows) {
+            this.board[y][x] = !this.board[y][x];
+        }
+        this.setState({ tiles: this.createTiles() });
+    }
+    componentWillUnmount() {  
+        clearInterval(this.state.intervalId);
+    }
+    render() {
+        const { tiles } = this.state;
+        return (
+        <div>
+            <div className="controls center aligned">
+                <button onClick={this.runGame} className='ui button primary'>Start</button>
+                <button onClick={this.stopGame} className='ui button red'>Stop</button>
+                <button onClick={this.makeRandomBoard} className='ui button green'>Random Board</button>
+                
+            </div>
+            <div 
+            className="Board"
+            style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${TILE_SIZE}px ${TILE_SIZE}px`}}
+            onClick={this.handleClick}
+            ref={(n) => { this.boardRef = n; }}
+            >
+            {tiles.map(tile => (
+            <Tile 
+            tileSize={TILE_SIZE}
+            x={tile.x} 
+            y={tile.y}
+            key={`${tile.x},${tile.y}`}
+            />
+            ))}
+
+            </div>
+        </div>
+        );
+    }
+}
---
+++ src/Components/tile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './tile.css'
+
+export default ({x, y, tileSize}) => {
+      return (
+        <div 
+        className="tile" 
+        style={{
+                left: `${tileSize * x }px`,
+                top: `${tileSize * y }px`,
+                width: `${tileSize - 1}px`,
+                height: `${tileSize - 1}px`,
+            }}
+        />
+      );
+}
