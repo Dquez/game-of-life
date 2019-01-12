5b33dc189700a2444af3ed642cb87d55d43dec9e
Dariell Vasquez <dariellv@yahoo.com>
2019-01-11 21:17:56 -0500

    App.test complete

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,19 +1,22 @@
-// import React from "react";
-// import {shallow} from "enzyme";
-// import App from "components/App";
-// import CommentBox from "components/CommentBox";
-// import CommentList from "components/CommentList";
+import React from 'react';
+import {mount} from 'enzyme';
+import App from '../App';
+import Board from '../Board';
 
-// let wrapped;
-// beforeEach(() => {
-//     wrapped = shallow(<App />)   
-// })
-// it("shows a comment box", () =>{
-     
-//     //  wrapped.find(CommentBox) returns an array
-//      expect(wrapped.find(CommentBox).length).toEqual(1);
-// })
+let wrapped;
+beforeEach(() => {
+    wrapped = mount(
+    <App>
+        <Board />
+    </App>
+    )   
+})
 
-// it("shows a comment list", () =>{ 
-//     expect(wrapped.find(CommentList).length).toEqual(1);
-// })
+describe('App component', ()=>{
+    it('is a div with a class name of container', () =>{  
+        expect(wrapped.childAt(0).hasClass('container')).toEqual(true);        
+    })
+    it('renders a Board component', () =>{
+         expect(wrapped.contains(Board)).toEqual(true);
+    })
+})
--- src/components/__tests__/Board.test.js
+++ src/components/__tests__/Board.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import Board from '../Board';
-import Tile from '../tile';
+
 
 let wrapped;
-beforeEach(()=> wrapped = mount(<Board tileSize={32} width={1024} height={728}/>));
+beforeEach(()=> wrapped = shallow(<Board/>));
 afterEach(()=> wrapped.unmount());
 
 
