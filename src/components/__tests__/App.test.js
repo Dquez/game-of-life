129d0388033aa17822b5a7fcfe9bdd87640559c3
Dariell Vasquez <dariellv@yahoo.com>
2019-01-13 21:04:30 -0500

    folder /file name bug

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {mount} from 'enzyme';
-import App from '../app.js';
-import Board from '../board.js';
+import App from '../app';
+import Board from '../board';
 
 let wrapped;
 beforeEach(() => {
